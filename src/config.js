af87c88988464555875b4ac8251feb8e1516e187
Izunna Aneke <izunnaaneke@googlemail.com>
2020-05-21 19:34:20 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "eu-west-2",
+      BUCKET: "notes-app-uploads-ia"
+    },
+    apiGateway: {
+      REGION: "eu-west-2",
+      URL: "https://nxxzrygyo7.execute-api.eu-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-2",
+      USER_POOL_ID: "eu-west-2_LkLxsh8n1",
+      APP_CLIENT_ID: "5o28od06glmdgj4j0bcufjoo2f",
+      IDENTITY_POOL_ID: "eu-west-2:1ce6b8e7-083d-4466-9414-f59d09c738b8"
+    }
+  };
