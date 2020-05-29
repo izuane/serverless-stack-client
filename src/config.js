4aa64e674920c4a76d0b949eee4d26fd5f3a6228
Izunna Aneke <izunnaaneke@googlemail.com>
2020-05-29 21:43:58 +0100

    Add CreateNote

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 export default {
+    MAX_ATTACHMENT_SIZE: 5000000,
     s3: {
       REGION: "eu-west-2",
       BUCKET: "notes-app-uploads-ia"
     },
     apiGateway: {
       REGION: "eu-west-2",
-      URL: "https://nxxzrygyo7.execute-api.eu-west-2.amazonaws.com/prod"
+      URL: "https://fheplk9bea.execute-api.eu-west-2.amazonaws.com/prod"
     },
     cognito: {
       REGION: "eu-west-2",
